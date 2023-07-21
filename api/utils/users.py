66e4ecaa3bb584f0cf5d959ed0cf713eb0b83ee6
jason-greenberg <jasongreenberg07@gmail.com>
2023-07-21 16:53:10 -0700

    Create user pydantic schema

---
+++ api/utils/users.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+
+from db.models.user import User
+from pydantic_schemas.user import UserCreate
+
+# get user by id
+def get_user(db: Session, user_id: int):
+    return db.query(User).get(user_id)
+
+# get user by email
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+# get users list
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+# create user
+def create_user(db: Session, user: UserCreate):
+    db_user = User(email=user.email, role=user.role)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
