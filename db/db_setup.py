22d331947acf8efba89f04186decb1b0019a37a6
jason-greenberg <jasongreenberg07@gmail.com>
2023-07-21 15:23:29 -0700

    Create db_setup.py w/ db utility function get_db

---
+++ db/db_setup.py
@@ -0,0 +1,37 @@
+import os
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+environment = os.getenv("ENV")
+SCHEMA = os.getenv("SCHEMA")
+
+# SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
+    # (only 'postgresql') but heroku's postgres add-on automatically sets the
+    # url in the hidden config vars to start with postgres.
+    # so the connection uri must be updated here (for production)
+SQLALCHEMY_DATABASE_URI = os.environ.get(
+    'DATABASE_URL').replace('postgres://', 'postgresql://')
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI, connect_args={}, future=True
+)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
+
+Base = declarative_base()
+
+# DB Utilities
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
