b579c54cc881001dbf433391f2641e0654b10c7d
jason-greenberg <jasongreenberg07@gmail.com>
2023-07-23 16:17:03 -0700

    Add functionality to seed permit data from csv file

--- db/seeds/permits.py
+++ db/seeds/permits.py
@@ -1,28 +1,52 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
-from db.models.user import User
+from db.models.permit import MobileFoodFacilityPermit
 from db.db_setup import environment, SCHEMA
-from api.utils.users import create_user
+from api.utils.permits import create_permit
+from pydantic_schemas.permit import PermitCreate
 
-def seed_users(db: Session):
-    users = [
-        User(email='demo@radai.com', password='password', is_active=True),
-        # Add more users as necessary
-    ]
+import csv
+from datetime import datetime
 
-    for user in users:
-        create_user(db=db, user=user)
-    
-    db.flush()  # Flush the changes without committing to make them available in the same session
+def seed_permits(db: Session):
+    with open('mobile_food_facility_permit.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        for row in reader:
+            # Convert date strings to datetime objects
+            approved = datetime.strptime(row[19], '%m/%d/%Y %I:%M:%S %p')
+            received = datetime.strptime(row[21], '%Y%m%d')
+            expiration_date = datetime.strptime(row[23], '%m/%d/%Y %I:%M:%S %p')
 
-def undo_users(db: Session):
+            # Create the PermitCreate instance
+            permit_create = PermitCreate(
+                location_id=row[0],
+                applicant=row[1],
+                facility_type=row[2],
+                cnn=row[3],
+                location_description=row[4],
+                address=row[5],
+                blocklot=row[6],
+                block=row[7],
+                lot=row[8],
+                permit=row[9],
+                status=row[10],
+                food_items=row[11],
+                x=row[12],
+                y=row[13],
+                schedule=row[16],
+                NOISent=row[18],
+                approved=approved,
+                received=received,
+                prior_permit=row[22],
+                expiration_date=expiration_date,
+            )
+
+            # Create the permit
+            create_permit(db=db, permit=permit_create)
+
+def undo_permits(db: Session):
     if environment == "production":
-        db.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.execute(f"TRUNCATE table {SCHEMA}.mobile_food_facility_permits RESTART IDENTITY CASCADE;")
     else:
-        db.execute(text("DELETE FROM users"))
-
-def undo_profiles(db: Session):
-    if environment == "production":
-        db.execute(f"TRUNCATE table {SCHEMA}.profiles RESTART IDENTITY CASCADE;")
-    else:
-        db.execute(text("DELETE FROM profiles"))
+        db.execute(text("DELETE FROM mobile_food_facility_permits"))
--- db/seeds/seed.py
+++ db/seeds/seed.py
@@ -3,6 +3,7 @@ import typer
 from db.db_setup import SessionLocal
 
 from db.seeds.users import seed_users, undo_users
+from db.seeds.permits import seed_permits, undo_permits
 
 app = typer.Typer()
 
@@ -11,6 +12,7 @@ def seed():
     db = SessionLocal()
     try:
         seed_users(db)
+        seed_permits(db)
         db.commit()
     finally:
         db.close()
@@ -20,6 +22,7 @@ def undo():
     db = SessionLocal()
     try:
         undo_users(db)
+        undo_permits(db)
         db.commit()
     finally:
         db.close()
