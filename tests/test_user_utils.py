518d95c63fd63689c3ac8586a2b5743297fadd27
jason-greenberg <jasongreenberg07@gmail.com>
2023-07-24 11:41:45 -0700

    Modify test_user_utils.py

--- tests/test_user_utils.py
+++ tests/test_user_utils.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import sessionmaker, Session
 
 from db.models.user import User
 from pydantic_schemas.user import UserCreate
-from api.utils.user import get_user, get_user_by_email, get_users, create_user
+from api.utils.users import get_user, get_user_by_email, get_users, create_user
 
 import os
+from random import randint
 
 # Set up a test database
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
@@ -26,13 +27,13 @@ def test_db() -> Session:
         session.close()
 
 def test_get_user(test_db):
-    user_id = 1  # Set this to an existing user ID
+    user_id = 2  # Set this to an existing user ID
     user = get_user(test_db, user_id)
     assert user is not None
     assert user.id == user_id
 
 def test_get_user_by_email(test_db):
-    email = "testuser@example.com"  # Set this to an existing user email
+    email = "demo@radai.com"  # Set this to an existing user email
     user = get_user_by_email(test_db, email)
     assert user is not None
     assert user.email == email
@@ -42,7 +43,7 @@ def test_get_users(test_db):
     assert len(users) >= 0  # As the function will always return a list
 
 def test_create_user(test_db):
-    user_data = UserCreate(email="newuser@example.com", password="newuserpassword", is_active=True)
+    user_data = UserCreate(email=f"newuser{randint(1,10000)}@example.com", password="newuserpassword", is_active=True)
     user = create_user(test_db, user_data)
     assert user is not None
     assert user.email == user_data.email
